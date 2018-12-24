bc37e3cd8aee7b95af16d1a2d7dcdd5617a25a1a
Fran Borja <franborjavalero@gmail.com>
2018-12-24 17:15:07 +0100

    added complete name of author

--- npostagging/nn.py
+++ npostagging/nn.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2018  Fran-Borja Valero <franborjavalero@gmail.com>
+# Copyright (C) 2018 Francisco de Borja Valero <franborjavalero@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
--- preprocess_synthetic.sh
+++ preprocess_synthetic.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (C) 2018  Fran-Borja Valero <franborjavalero@gmail.com>
+# Copyright (C) 2018 Francisco de Borja Valero <franborjavalero@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
